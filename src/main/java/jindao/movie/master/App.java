65e2abb8cf13d909e00708fa4014c3a49fbcf23e
AmosJindao <taianrizhao@163.com>
2013-11-17 10:41:50 +0800

    测试
    Signed-off-by: AmosJindao <taianrizhao@163.com>

---
+++ src/main/java/jindao/movie/master/App.java
@@ -0,0 +1,13 @@
+package jindao.movie.master;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
