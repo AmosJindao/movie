4fa17047f572efde2a5db68a7fa8377dc52e30f7
amos <liusr@outlook.com>
2014-05-03 19:45:35 +0800

    初步定義用戶信息

---
+++ movie-web/src/main/java/org/movie/bean/User.java
@@ -0,0 +1,13 @@
+package org.movie.bean;
+
+import java.io.Serializable;
+
+/**
+ * @author amos
+ * @date May 3, 2014 7:23:51 PM
+ */
+public class User implements Serializable{
+	
+	private String name;
+	private int age;
+}
