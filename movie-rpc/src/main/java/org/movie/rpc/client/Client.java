e82e19f860e132ab76d0453afd869ffe3c02e67b
amos <liusr@outlook.com>
2014-09-10 21:37:57 +0800

    rpc的server和client

---
+++ movie-rpc/src/main/java/org/movie/rpc/client/Client.java
@@ -0,0 +1,9 @@
+package org.movie.rpc.client;
+
+/**
+ * @author amos
+ * @since 1.0, Sep 4, 2014 10:17:57 PM
+ */
+public class Client {
+
+}
---
+++ movie-rpc/src/main/java/org/movie/rpc/server/Server.java
@@ -0,0 +1,9 @@
+package org.movie.rpc.server;
+
+/**
+ * @author amos
+ * @since 1.0, Sep 4, 2014 10:18:04 PM
+ */
+public class Server {
+
+}
