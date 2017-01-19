57f3ab693b4ff4d8c6adbb966bd9b791ecee0b45
liushengri <forsecurity668@163.com>
2017-01-19 16:11:32 +0800

    lamdba throw exception\

--- tests/src/main/java/tests/jdk8/date/ClockTest.java
+++ tests/src/main/java/tests/jdk8/date/ClockTest.java
@@ -3,7 +3,7 @@ package tests.jdk8.date;
 import java.time.Clock;
 
 /**
- * Created by liushengri on 2017/1/19.
+ * Created by on 2017/1/19.
  */
 public class ClockTest {
     public static void main(String[] args) {
