bcb4a532d48d9f22626ea8a0a46f9df33ec7bf44
liushengri <forsecurity668@163.com>
2017-03-24 16:22:10 +0800

    test

---
+++ tests/src/main/java/tests/math/Plus.java
@@ -0,0 +1,14 @@
+package tests.math;
+
+/**
+ * Created by liushengri on 2017/3/16.
+ */
+public class Plus {
+    public static void main(String[] args) {
+        long hundrudYears = 100l*365l*24l*60l*60l*1000l;
+        System.out.println(hundrudYears);
+        System.out.println(1489645286000l+hundrudYears);
+        
+        
+    }
+}
---
+++ tests/src/main/java/tests/thread/ThreadTest1.java
@@ -0,0 +1,72 @@
+package tests.thread;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * two threads, thread A generates 5 random numbers per second, the number range is [0,100].
+ * While thread B reads the A-produced numbers every two seconds, and sorts then prints out.
+ * <p>
+ * Created by liushengri on 2017/3/23.
+ */
+public class ThreadTest1 {
+    static Queue<Integer> sque = new LinkedList<>();
+
+    public static void main(String[] args) {
+        new Thread(new ThreadA()).start();
+        new Thread(new ThreadB()).start();
+       
+    }
+
+    static class ThreadA implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                }
+
+                Random rd = new Random();
+                
+                for(int i=0; i<5; i++){
+                    sque.offer(rd.nextInt(101));
+                }
+            }
+        }
+    }
+
+    static class ThreadB implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                }
+
+                List<Integer> numList = new LinkedList<>();
+                
+                Integer tmpNum = sque.poll();
+                while (tmpNum != null){
+                    numList.add(tmpNum);
+                    
+                    tmpNum = sque.poll();
+                }
+
+                Integer[] numArr = new Integer[numList.size()];
+                
+                Arrays.sort(numList.toArray(numArr));
+
+                for(Integer tmp:numArr) {
+                    System.out.print(tmp+"\t");
+                }
+
+                System.out.println();
+                System.out.println();
+            }
+        }
+    }
+}
